8b04b67c64afa55e4a1e2373db446a4ab68da510
xianvy <xianvy0000@gmail.com>
2025-03-29 09:22:27 +0800

    Feature : Add visibility toggle for table list

--- components/features/table-list.tsx
+++ components/features/table-list.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react'
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { ExpandButton } from "@/components/ui/expand-button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { tableData } from '@/data/tables';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { Button } from '../ui/button';
 
 const TableList = () => {
     const [expandedTable, setExpandedTable] = useState<string | null>(null);
+    const [visibleTables, setVisibleTables] = useState<Set<string>>(new Set());
 
     return Object.entries(tableData).map(([tableName, data]) => {
+        type RowType = typeof data[0];
         const columns = data.length > 0 ? Object.keys(data[0]) : [];
         const isExpanded = expandedTable === tableName;
+        const isVisible = visibleTables.has(tableName);
         
         return (
-          <Card key={tableName} className={`p-4  border-1 gap-2 !shadow-none ${isExpanded ? 'fixed inset-4 z-50 bg-card' : ''}`}>
+          <Card key={tableName} className={`p-4 border-1 gap-2 !shadow-none relative ${isExpanded ? 'fixed inset-4 z-50 bg-card' : ''}`}>
             <CardHeader className="flex flex-row items-center justify-between px-2">
               <CardTitle>{tableName}</CardTitle>
               <ExpandButton 
@@ -22,42 +27,64 @@ const TableList = () => {
               />
             </CardHeader>
             
-            <ScrollArea className={isExpanded ? 'h-[80vh]' : 'h-48'}>
-              <ScrollArea className="w-full" >
+            <ScrollArea className={`${isExpanded ? 'h-[80vh]' : isVisible ? 'h-48' : 'h-12'}`}>
+              <ScrollArea className="w-full">
                 <div className="text-xs">
                   <Table>
                     <TableHeader>
-                    <TableRow>
-                      {columns.map((column) => {
-                        const isPrimaryKey = column.toLowerCase().includes('id') && column.length === 2;
-                        const isForeignKey = column.toLowerCase().includes('id') && column.length > 2;
-                        return (
-                          <TableHead key={column} className="whitespace-nowrap">
-                            {isPrimaryKey && '🔑 '}
-                            {isForeignKey && '🔗 '}
-                            {column}
-                          </TableHead>
-                        );
-                      })}
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {data.map((row, index) => (
-                      <TableRow key={index}>
-                        {columns.map((column) => (
-                          <TableCell key={column}>
-                            {row[column] === null ? 'NULL' : String(row[column])}
-                          </TableCell>
-                        ))}
+                      <TableRow>
+                        {columns.map((column) => {
+                          const isPrimaryKey = column.toLowerCase().includes('id') && column.length === 2;
+                          const isForeignKey = column.toLowerCase().includes('id') && column.length > 2;
+                          return (
+                            <TableHead key={column} className="whitespace-nowrap">
+                              {isPrimaryKey && '🔑 '}
+                              {isForeignKey && '🔗 '}
+                              {column}
+                            </TableHead>
+                          );
+                        })}
                       </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </div>
-
-            </ScrollArea>
+                    </TableHeader>
+                    {(isVisible || isExpanded) && (
+                      <TableBody>
+                        {data.map((row, index) => (
+                          <TableRow key={index}>
+                            {columns.map((column) => (
+                              <TableCell key={column}>
+                             {columns.map((column) => (
+                                  <TableCell key={column}>
+                                    {row[column as keyof RowType] === null ? 'NULL' : String(row[column as keyof RowType])}
+                                  </TableCell>
+                                ))}
+                              </TableCell>
+                            ))}
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    )}
+                  </Table>
+                </div>
+              </ScrollArea>
               <ScrollBar orientation="horizontal" />
             </ScrollArea>
+           <CardFooter className='px-2 justify-end'>
+                <Button 
+                  onClick={() => setVisibleTables(prev => {
+                    const newSet = new Set(prev);
+                    if (isVisible) {
+                      newSet.delete(tableName);
+                    } else {
+                      newSet.add(tableName);
+                    }
+                    return newSet;
+                  })}
+                  variant="ghost"   
+                  className="h-7 w-7 cursor-pointer bg-muted "
+                >
+                  {isVisible ? <ChevronUp strokeWidth={1.5} className="!w-3.5 !h-3.5" /> : <ChevronDown strokeWidth={1.5} className="!w-3.5 !h-3.5" />}
+                </Button>
+            </CardFooter>
           </Card>
         );
       });
