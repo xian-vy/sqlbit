fac78046604fed793f4ee9fb39c2c591d4fafc01
xianvy <xianvy0000@gmail.com>
2025-04-01 21:26:30 +0800

    Reduce hero/about text size in mobile screen

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,19 +8,19 @@ subsets: ["latin"],
 
 const page = () => {
   return (
-    <div className={`flex-1 flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 min-h-screen ${pxel.className}`}>
+    <div className={`flex-1 flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 min-h-[100dvh] ${pxel.className}`}>
     <div className="text-center space-y-8">
-      <h1 className="text-6xl sm:text-7xl lg:text-8xl font-light tracking-tighter font-pixelify">
+      <h1 className="text-5xl sm:text-7xl lg:text-8xl font-light tracking-tighter font-pixelify">
         <span className="text-[#FF8C00]">SQL</span>
         <span className="text-slate-100">BIT</span>
       </h1>
-      <p className="text-slate-400 text-base sm:text-base 2xl:text-lg max-w-2xl mx-auto">
+      <p className="text-slate-400 text-sm sm:text-base 2xl:text-lg max-w-2xl mx-auto">
         Developed by <a href="https://xianvy.vercel.app" target="_blank" rel="noopener noreferrer" className="text-[#FF8C00] hover:text-[#FF9F33] transition-colors duration-200">Xian Vy</a>
         </p>
       <div className="mt-8">
         <Link
           href="/app"
-          className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-slate-900 bg-[#FF8C00] hover:bg-[#FF9F33] transition-colors duration-200"
+          className="inline-flex items-center px-4 py-2  border border-transparent text-sm sm:text-base font-medium rounded-md text-slate-900 bg-[#FF8C00] hover:bg-[#FF9F33] transition-colors duration-200"
         >
           Back to Editor
         </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,17 @@ export default function Landing() {
     <div className={`flex flex-col min-h-screen bg-slate-900 ${pxel.className} font-pixelify antialiased`}>
       <main className="flex-1 flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
         <div className="text-center space-y-8">
-          <h1 className="text-6xl sm:text-7xl lg:text-8xl font-light tracking-tighter font-pixelify">
+          <h1 className="text-5xl sm:text-7xl lg:text-8xl font-light tracking-tighter font-pixelify">
             <span className="text-[#FF8C00]">SQL</span>
             <span className="text-slate-100">BIT</span>
           </h1>
-          <p className="text-slate-400 text-base sm:text-base 2xl:text-lg max-w-2xl mx-auto">
+          <p className="text-slate-400 text-sm sm:text-base 2xl:text-lg max-w-2xl mx-auto px-4 sm:px-0">
             No Fuss SQL Playground with prebuilt queries and tables.
           </p>
           <div className="mt-8">
             <Link
               href="/app"
-              className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-slate-900 bg-[#FF8C00] hover:bg-[#FF9F33] transition-colors duration-200"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-sm sm:text-base font-medium rounded-md text-slate-900 bg-[#FF8C00] hover:bg-[#FF9F33] transition-colors duration-200"
             >
               Try it out
             </Link>
