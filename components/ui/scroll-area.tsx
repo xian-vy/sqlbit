ffdf955d8cce7eb978635293bd7aa3c36308dda9
jovie00 <xianvy0000@gmail.com>
2025-04-03 13:15:26 +0800

    Change scrollbar style

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -38,7 +38,7 @@ function ScrollBar({
       className={cn(
         "flex touch-none p-px transition-colors select-none",
         orientation === "vertical" &&
-          "h-full w-2.5 border-l border-l-transparent",
+          "h-full w-[7px] border-l border-l-transparent",
         orientation === "horizontal" &&
           "h-2.5 flex-col border-t border-t-transparent",
         className
@@ -47,7 +47,7 @@ function ScrollBar({
     >
       <ScrollAreaPrimitive.ScrollAreaThumb
         data-slot="scroll-area-thumb"
-        className="bg-slate-600 relative flex-1 rounded-full"
+        className="bg-slate-700 relative flex-1 rounded-full"
       />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
   )
