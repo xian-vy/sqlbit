7c8be6dea123b6471847bf40ea8a1db3c37a7d79
xianvy <xianvy0000@gmail.com>
2025-07-13 12:35:45 +0800

    Feat : link portfolio in landing

--- app/page.tsx
+++ app/page.tsx
@@ -17,19 +17,24 @@ export default function Landing() {
             <span className="text-[#FF8C00]">SQL</span>
             <span className="text-slate-900 dark:text-slate-100">BIT</span>
           </h1>
-          <p className="text-slate-900 dark:text-slate-400 text-sm sm:text-base 2xl:text-lg max-w-2xl mx-auto px-4 sm:px-0">
+          <p className="text-slate-900 dark:text-slate-300 text-sm sm:text-base md:text-lg 2xl:text-xl max-w-2xl mx-auto px-4 sm:px-0">
             No Fuss SQL Playground with prebuilt queries and tables.
           </p>
           <div className="mt-8">
             <Link
               href="/app"
-              className="inline-flex items-center px-4 py-2 border border-transparent text-sm sm:text-base font-medium rounded-md text-white dark:text-slate-900  bg-[#FF8C00] hover:bg-[#FF9F33] transition-colors duration-200"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-sm sm:text-base font-semibold rounded-md text-white dark:text-slate-900  bg-[#FF8C00] hover:bg-[#FF9F33] transition-colors duration-200"
             >
-              Try it out
+              TRY IT OUT
             </Link>
           </div>
         </div>
       </main>
+      <footer>
+          <p className="text-slate-900 dark:text-slate-400  text-xs sm:text-sm absolute left-1/2 bottom-5 -translate-x-1/2">
+            Developed by <Link href="https://xianvy.vercel.app" target="_blank" rel="noopener noreferrer" className="text-[#FF8C00] hover:text-[#FF9F33] transition-colors duration-200">XIAN VY</Link>
+         </p>
+      </footer>
     </div>
   );
 }
